6213c430b05d406e684b7bba81972c103a9f6ad5
Ilhui <ilhuicatl.id@gmail.com>
2019-10-19 08:54:23 -0500

    Ultimos detalles de fotografias e interactivaida

--- js/script.js
+++ js/script.js
@@ -136,3 +136,14 @@ span6.onclick = function() {
 
 
 // Active link
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
