f575447fc619b8f0fa51fca903be7f79e0c4bdbf
Ilhui <ilhuicatl.id@gmail.com>
2019-10-25 02:45:52 -0500

    Agramos validación de formulario

---
+++ js/form.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  $('#btn_send').click(function(){
+    var errores = '';
+
+    // validando el nombre
+    if( $('#names').val() == '' ){
+      errores += '<p>Escriba un nombre.</p>';
+      $('#names').css("border-bottom-color","#F14b3b")
+    }else{
+      $('#names').css("border-bottom-color","#FFF")
+    }
+    // validando el correo
+    if( $('#email').val() == '' ){
+      errores += '<p>Ingrese un correo.</p>';
+      $('#email').css("border-bottom-color","#F14b3b")
+    }else{
+      $('#email').css("border-bottom-color","#FFF")
+    }
+    // validando el mensaje
+    if( $('#mensaje').val() == '' ){
+      errores += '<p>Escriba un mensaje.</p>';
+      $('#mensaje').css("border-bottom-color","#F14b3b")
+    }else{
+      $('#mensaje').css("border-bottom-color","#FFF")
+    }
+
+    // Enviando mensaje
+
+    if(errores == '' == false) {
+      var mensajeModal = '<div class="modal_wrap">' +
+                            '<div class="mensaje_modal">'+
+                              '<h3>Errores encontrados</h3>'+
+                              errores +
+                              '<span id="btn_close">Cerrar</span>'+
+                            '</div>'+
+                          '</div>'
+      $('body').append(mensajeModal);
+    }
+
+    // Cerrando el modal_wrap
+    $('#btn_close').click(function(){
+      $('.modal_wrap').remove();
+
+    });
+
+  });
+
+});
--- js/script.js
+++ js/script.js
@@ -211,3 +211,6 @@ for (var i = 0; i < btns.length; i++) {
   this.className += " active";
   });
 }
+
+
+// 
